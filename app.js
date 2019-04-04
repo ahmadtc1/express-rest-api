8c230fd45471c5584fcc5dec0763a33fd3e40487
ahmadtc1 <ahmadtc2000@gmail.com>
2019-04-03 20:19:55 -0400

    Connecting to db with mongoose

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const bookRouter = require('./src/routes/bookRoutes');
 //Initializing the express application
 const app = express();
-
+const mongoose = require('mongoose');
 //Assigning a port to the server
 const port = process.env.PORT || 4000;
+
+const bookRouter = express.Router();
+
+bookRouter.route('/Books')
+    .get((req, res) => {
+        let responseJson = {hello: "This is my api"};
+        res.json(responseJson);
+    })
 
 app.use('/api', bookRouter);
 
