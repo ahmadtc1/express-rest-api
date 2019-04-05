a9132b9c5a22af599cf1af22ca2d181ed88bf078
ahmadtc1 <ahmadtc2000@gmail.com>
2019-04-05 19:54:45 -0400

    debugging

--- app.js
+++ app.js
@@ -6,13 +6,19 @@ const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
 
+const db = mongoose.connect('mongodb://localhost/bookAPI');
+
 //Assigning a port to the server
 const port = process.env.PORT || 4000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
 const bookRouter = express.Router();
 
+app.use('/api', bookRouter);
 
-const db = mongoose.connect('mongodb://localhost/bookAPI');
 const book = require('./models/bookModel');
 
 bookRouter.route('/Books')
@@ -24,20 +30,23 @@ bookRouter.route('/Books')
             query.genre = req.query.genre;
             query.title = req.query.title;
             query.author = req.query.author;
-            book.find(query, (err, books) => {
-                if (err) {
-                    res.status(500).send(err);
-                }
-                else {
-                    res.json(books);
-                }
-            })
         }
+        book.find(query, (err, books) => {
+            if (err) {
+                res.status(500).send(err);
+            }
+            else {
+                res.json(books);
+            }
+        })
+
     })
 
     .post((req, res) => {
-        let book = new book();
+        let book = new book(req.body);
 
+        console.log(book);
+        res.send(book);
     })
 
 bookRouter.route('/books/:bookId')
@@ -52,7 +61,6 @@ bookRouter.route('/books/:bookId')
         })
     })
 
-app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
     res.send("Welcome to my API!");
