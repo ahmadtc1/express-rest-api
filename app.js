a93e8df97aa6c52d0fa333789cacb5b1cc2517b4
ahmadtc1 <ahmadtc2000@gmail.com>
2019-04-05 21:35:44 -0400

    Restructuring code files and cleaning code

--- app.js
+++ app.js
@@ -8,59 +8,20 @@ const mongoose = require('mongoose');
 
 const db = mongoose.connect('mongodb://localhost/bookAPI');
 
+const Book = require('./models/bookModel');
+
 //Assigning a port to the server
 const port = process.env.PORT || 4000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-const bookRouter = express.Router();
+bookRouter = require('./src/routes/bookRoutes')(Book);
 
 app.use('/api', bookRouter);
 
-const Book = require('./models/bookModel');
 
-bookRouter.route('/Books')
-    .get((req, res) => {
-        let responseJson = { hello: "This is my api" };
-        var query = {};
-        //Ensuring that the user is not just sending in a random query
-        if (req.query.genre || req.query.author || req.query.title) {
-            query.genre = req.query.genre;
-            query.title = req.query.title;
-            query.author = req.query.author;
-        }
-        Book.find(query, (err, books) => {
-            if (err) {
-                res.status(500).send(err);
-            }
-            else {
-                res.json(books);
-            }
-        })
 
-    })
-
-    .post((req, res) => {
-        let book = new Book(req.body);
-        book.save();
-
-        console.log(book);
-        res.status(201).send(book);
-    })
-
-bookRouter.route('/books/:bookId')
-    .get((req, res) => {
-        Book.findById(req.params.bookId, (err, book) => {
-            if (err) {
-                res.status(500).send(err);
-            }
-            else {
-                res.json(book);
-            }
-        })
-    })
 
 
 app.get('/', (req, res) => {
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,15 +1,49 @@
 const express = require('express');
 
-const bookRouter = express.Router();
 
-function router() {
-    bookRouter.route('/books')
+function router(Book) {
+    const bookRouter = express.Router();
+    bookRouter.route('/Books')
         .get((req, res) => {
-            let responseJson = {hello: "This is my API"};
-            res.send(responseJson);
+            let responseJson = { hello: "This is my api" };
+            var query = {};
+            //Ensuring that the user is not just sending in a random query
+            if (req.query.genre || req.query.author || req.query.title) {
+                query.genre = req.query.genre;
+                query.title = req.query.title;
+                query.author = req.query.author;
+            }
+            Book.find(query, (err, books) => {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                else {
+                    res.json(books);
+                }
+            })
+
         })
 
+        .post((req, res) => {
+            let book = new Book(req.body);
+            book.save();
 
+            console.log(book);
+            res.status(201).send(book);
+        })
+
+    bookRouter.route('/books/:bookId')
+        .get((req, res) => {
+            Book.findById(req.params.bookId, (err, book) => {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                else {
+                    res.json(book);
+                }
+            })
+        })
+    return bookRouter;
 }
 
 module.exports = router;
